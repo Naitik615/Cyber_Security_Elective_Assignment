f2cfee8dd60023e94c0375d76c3c61a5dbd85564
Naitik615 <naitikdhiman06@gmail.com>
2026-05-13 01:54:56 +0530

    Create Assignment_1-Q_11.sh

---
+++ Assignment_1/Assignment_1-Q_11.sh
@@ -0,0 +1,12 @@
+students = {
+    "Alice": 85,
+    "Bob": 90,
+    "Charlie": 78,
+    "David": 88,
+    "Eva": 92
+}
+
+total = sum(students.values())
+average = total / len(students)
+
+print("Average Marks:", average)
